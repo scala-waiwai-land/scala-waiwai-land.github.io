6cf60a365add4c5c48d520c3afd934a47443f736
Windymelt <windymelt@3qe.us>
2024-06-14 04:18:03 +0900

    notrailingslash

--- scala-waiwai-land/docusaurus.config.ts
+++ scala-waiwai-land/docusaurus.config.ts
@@ -18,6 +18,7 @@ const config: Config = {
   organizationName: 'scala-waiwai-land', // Usually your GitHub org/user name.
   projectName: 'scala-waiwai-land.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
